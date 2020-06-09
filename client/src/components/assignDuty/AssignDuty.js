0326f6d729a6a17811a20424b5815910ae6f56b9
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:27:43 +0530

    assign duty component added

---
+++ client/src/components/assignDuty/AssignDuty.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getClients, getProspects } from "../../actions/client";
+import Spinner from "../layout/Spinner";
+import { getUsers } from "../../actions/auth";
+import ClientDuty from "./ClientDuty";
+import ProspectDuty from "./ProspectDuty";
+
+const AssignDuty = ({
+	getClients,
+	getUsers,
+	getProspects,
+	client: { clients, prospects, loading },
+	users,
+}) => {
+	useEffect(() => {
+		getClients();
+		getProspects();
+		getUsers();
+	}, [getClients, getProspects, getUsers]);
+
+	return loading ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<ClientDuty clients={clients} users={users} />
+			<ProspectDuty prospects={prospects} users={users} />
+		</Fragment>
+	);
+};
+
+AssignDuty.propTypes = {
+	getClients: PropTypes.func.isRequired,
+	getProspects: PropTypes.func.isRequired,
+	getUsers: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	client: state.client,
+	users: state.auth.users,
+});
+
+export default connect(mapStateToProps, { getClients, getProspects, getUsers })(
+	AssignDuty
+);
