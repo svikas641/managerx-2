92d1b3f7c12f8995bd295e60f0efdf12b037ca20
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:23:43 +0530

    added reducer client and duty

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,14 @@ import alert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
 import lead from "./lead";
+import client from "./client";
+import duty from "./duty";
 
 export default combineReducers({
 	alert,
 	auth,
 	profile,
 	lead,
+	client,
+	duty,
 });
