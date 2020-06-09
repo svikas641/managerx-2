2fbd1892b3f8b04c5b4c71804cc8bd49763ae0d6
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:25:57 +0530

    added get all user end point

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,79 +1,99 @@
-const express =  require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const config = require('config');
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const checkRole = require("../../middleware/checkRole");
+const auth = require("../../middleware/auth");
 
-const user = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Test Route
 // @access  Public
 
-router.post('/',[
-    check('name', 'Name is required')
-      .not()
-      .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('phoneNumber', 'Please include a valid phoneNumber').isLength({ min: 10}),
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("phoneNumber", "Please include a valid phoneNumber").isLength({
+      min: 10,
+    }),
     check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
   ],
-  async (req,res)=>{
-  	const errors = validationResult(req);
+  async (req, res) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const { name,email,phoneNumber,password } = req.body;
+    const { name, email, phoneNumber, password } = req.body;
     console.log(req.body);
-    
+
     try {
       // See if user exists
-    
+
       let user = await User.findOne({ email });
 
-      if(user){
-        return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       user = new User({
         name,
         email,
         phoneNumber,
-        password
+        password,
       });
 
-    // Encrypt Password
+      // Encrypt Password
 
-    const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-    user.password = await bcrypt.hash(password, salt);
-    await user.save();
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
 
-    //Return jsonwebtokens
-    const payload = {
-      user: {
-        id: user.id
-      }
-    }
+      //Return jsonwebtokens
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-    jwt.sign(
-      payload,
-      config.get('jwtSecret'),
-      {expiresIn: 3600},
-      (err, token) =>{
-        if(err) throw err;
-        res.json({ token });
-      });
-
-    } catch(e) {
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (e) {
       // statements
       console.log(e.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
-})
+  }
+);
+
+// @route    GET api/users
+// @desc     Get all users
+// @access   Public
+router.get("/", auth, checkRole, async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
