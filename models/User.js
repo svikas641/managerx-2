33aabcb09bbab94811b13dbff52a08fa665d8e07
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:20:10 +0530

    added role in model

--- models/User.js
+++ models/User.js
@@ -1,27 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
   },
   phoneNumber: {
     type: Number,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model("user", UserSchema);
