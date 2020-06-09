b9063d038c6d0147b80a982610611be92511ffd7
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:23:26 +0530

    all users added

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,47 +5,55 @@ import {
 	AUTH_ERROR,
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
-	LOGOUT
-} from '../actions/types';
+	LOGOUT,
+	ALL_USERS,
+} from "../actions/types";
 
 const initialState = {
-	token: localStorage.getItem('token'),
+	token: localStorage.getItem("token"),
 	isAuthenticated: null,
 	loading: true,
-	user: null
-}
+	user: null,
+	users: [],
+};
 
-export default function(state = initialState, action){
-
-	const {type, payload} = action;
+export default function (state = initialState, action) {
+	const { type, payload } = action;
 
 	switch (type) {
 		case USER_LOADED:
-			return{
+			return {
 				...state,
 				isAuthenticated: true,
 				loading: false,
-				user: payload
-			}
+				user: payload,
+			};
 		case REGISTER_SUCCESS:
 		case LOGIN_SUCCESS:
-			localStorage.setItem('token', payload.token);
+			localStorage.setItem("token", payload.token);
 			return {
 				...state,
 				...payload,
 				isAuthenticated: true,
-				loading: false
+				loading: false,
 			};
 		case REGISTER_FAIL:
 		case AUTH_ERROR:
 		case LOGIN_FAIL:
 		case LOGOUT:
-			localStorage.removeItem('token');
+			localStorage.removeItem("token");
 			return {
 				...state,
 				token: null,
 				isAuthenticated: false,
-				loading: false
+				loading: false,
+			};
+		case ALL_USERS:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				users: payload,
 			};
 		default:
 			return state;
