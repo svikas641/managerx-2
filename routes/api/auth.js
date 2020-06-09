398c9023a0568bb0ff938424572460a05edaf46d
Vikas_Kumar <svikas641@gmail.com>
2020-06-09 20:24:38 +0530

    removed sgmail middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-const sgmail = require("../../middleware/email");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
@@ -27,7 +26,6 @@ router.get("/", auth, async (req, res) => {
 // @access   Public
 router.post(
   "/",
-  sgmail,
   [
     check("email", "Please include a valid email").isEmail(),
     check("password", "Password is required").exists(),
