545df0011b9059997e72dddc07a315c7fa679d5f
Vikas_Kumar <svikas641@gmail.com>
2020-06-14 17:15:55 +0530

    Added multiselect functionality

--- client/src/components/assignDuty/ClientDuty.js
+++ client/src/components/assignDuty/ClientDuty.js
@@ -3,74 +3,76 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { assignClient } from "../../actions/duty";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+const animatedComponents = makeAnimated();
 
 const ClientDuty = ({ clients, users, assignClient, history }) => {
-	const initialState = {
-		client: "",
-		salesPerson: "",
-	};
-	const [formData, setFormData] = useState(initialState);
+  const initialState = {
+    salesPerson: "",
+  };
+  const [client, setClient] = useState([]);
 
-	const { client, salesPerson } = formData;
+  const [formData, setFormData] = useState(initialState);
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+  const { salesPerson } = formData;
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		assignClient(formData, history);
-		setFormData(initialState);
-	};
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value, client });
 
-	return (
-		<Fragment>
-			<div className="post-form">
-				<div className="bg-primary p">
-					<h3>For Client Visit</h3>
-				</div>
-				<form className="form my-1" onSubmit={onSubmit}>
-					<select
-						className="form-group"
-						name="client"
-						value={client}
-						onChange={onChange}
-					>
-						<option value="0">* Select Client</option>
-						{clients.map((client) => (
-							<option key={client._id} value={client._id}>
-								{client.companyName}
-							</option>
-						))}
-					</select>
-					<select
-						className="form-group"
-						name="salesPerson"
-						value={salesPerson}
-						onChange={onChange}
-					>
-						<option value="0">* Select Sales Person</option>
-						{users.map((user) => (
-							<option key={user._id} value={user._id}>
-								{user.name}
-							</option>
-						))}
-					</select>
-					<input
-						type="submit"
-						className="btn btn-dark my-1"
-						value="Submit"
-					/>
-					<Link className="btn btn-light my-1" to="/dashboard">
-						Go Back
-					</Link>
-				</form>
-			</div>
-		</Fragment>
-	);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    assignClient(formData, history);
+    setFormData(initialState);
+  };
+
+  const options = clients.map((element) => ({
+    label: element.clientName,
+    value: element._id,
+  }));
+
+  return (
+    <Fragment>
+      <div className="post-form">
+        <div className="bg-primary p">
+          <h3>For Client Visit</h3>
+        </div>
+        <form className="form my-1" onSubmit={onSubmit}>
+          <Select
+            name="client"
+            className="form-group"
+            onChange={setClient}
+            isMulti
+            options={options}
+            placeholder="Select Client"
+            components={animatedComponents}
+          />
+          <select
+            className="form-group"
+            name="salesPerson"
+            value={salesPerson}
+            onChange={onChange}
+          >
+            <option value="0">* Select Sales Person</option>
+            {users.map((user) => (
+              <option key={user._id} value={user._id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <input type="submit" className="btn btn-dark my-1" value="Submit" />
+          <Link className="btn btn-light my-1" to="/dashboard">
+            Go Back
+          </Link>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 ClientDuty.propTypes = {
-	assignClient: PropTypes.func.isRequired,
+  assignClient: PropTypes.func.isRequired,
 };
 
 export default connect(null, { assignClient })(ClientDuty);
--- client/src/components/assignDuty/ProspectDuty.js
+++ client/src/components/assignDuty/ProspectDuty.js
@@ -3,74 +3,77 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { assignProspect } from "../../actions/duty";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+const animatedComponents = makeAnimated();
 
 const ProspectDuty = ({ prospects, users, assignProspect, history }) => {
-	const initialState = {
-		prospect: "",
-		salesPerson: "",
-	};
-	const [formData, setFormData] = useState(initialState);
+  const initialState = {
+    salesPerson: "",
+  };
 
-	const { prospect, salesPerson } = formData;
+  const [prospect, setProspect] = useState([]);
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+  const [formData, setFormData] = useState(initialState);
 
-	const onSubmit = (e) => {
-		e.preventDefault();
-		assignProspect(formData, history);
-		setFormData(initialState);
-	};
+  const { salesPerson } = formData;
 
-	return (
-		<Fragment>
-			<div className="post-form">
-				<div className="bg-primary p">
-					<h3>For Prospect Visit</h3>
-				</div>
-				<form className="form my-1" onSubmit={onSubmit}>
-					<select
-						className="form-group"
-						name="prospect"
-						value={prospect}
-						onChange={onChange}
-					>
-						<option value="0">* Select Prospect</option>
-						{prospects.map((prospect) => (
-							<option key={prospect._id} value={prospect}>
-								{prospect.companyName}
-							</option>
-						))}
-					</select>
-					<select
-						className="form-group"
-						name="salesPerson"
-						value={salesPerson}
-						onChange={onChange}
-					>
-						<option value="0">* Select Sales Person</option>
-						{users.map((user) => (
-							<option key={user._id} value={user.name}>
-								{user.name}
-							</option>
-						))}
-					</select>
-					<input
-						type="submit"
-						className="btn btn-dark my-1"
-						value="Submit"
-					/>
-					<Link className="btn btn-light my-1" to="/dashboard">
-						Go Back
-					</Link>
-				</form>
-			</div>
-		</Fragment>
-	);
+  const options = prospects.map((element) => ({
+    label: element.prospectName,
+    value: element._id,
+  }));
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value, prospect });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    assignProspect(formData, history);
+    console.log(formData);
+    setFormData(initialState);
+  };
+
+  return (
+    <Fragment>
+      <div className="post-form">
+        <div className="bg-primary p">
+          <h3>For Prospect Visit</h3>
+        </div>
+        <form className="form my-1" onSubmit={onSubmit}>
+          <Select
+            name="prospect"
+            className="form-group"
+            onChange={setProspect}
+            isMulti
+            options={options}
+            placeholder="Select Prospect"
+            components={animatedComponents}
+          />
+          <select
+            className="form-group"
+            name="salesPerson"
+            value={salesPerson}
+            onChange={onChange}
+          >
+            <option value="0">* Select Sales Person</option>
+            {users.map((user) => (
+              <option key={user._id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <input type="submit" className="btn btn-dark my-1" value="Submit" />
+          <Link className="btn btn-light my-1" to="/dashboard">
+            Go Back
+          </Link>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 ProspectDuty.propTypes = {
-	assignProspect: PropTypes.func.isRequired,
+  assignProspect: PropTypes.func.isRequired,
 };
 
 export default connect(null, { assignProspect })(ProspectDuty);
