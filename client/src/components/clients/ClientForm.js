c7ff846941b4b0ecdd7292f16a45d6ce8016a524
Vikas_Kumar <svikas641@gmail.com>
2020-06-14 17:16:56 +0530

    added formik and fieldarray

--- client/src/components/clients/ClientForm.js
+++ client/src/components/clients/ClientForm.js
@@ -1,58 +1,112 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Formik, Field, Form, FieldArray } from "formik";
+import { createClient } from "../../actions/client";
 
-const ClientForm = () => {
-	const initialState = {
-		companyName: "",
-		clientAddress: "",
-	};
-	const [formData, setFormData] = useState(initialState);
-
-	const { companyName, clientAddress } = formData;
-
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-
-	const onSubmit = (e) => {
-		e.preventDefault();
-		console.log(formData);
-		setFormData(initialState);
-	};
-
-	return (
-		<Fragment>
-			<h1 className="large text-primary">Create Client</h1>
-			<form className="form" onSubmit={(e) => onSubmit(e)}>
-				<div className="form-group">
-					<input
-						type="text"
-						placeholder="Company Name"
-						name="companyName"
-						value={companyName}
-						onChange={(e) => onChange(e)}
-						required
-					/>
-				</div>
-				<div className="form-group">
-					<input
-						type="text"
-						placeholder="Client Address"
-						name="clientAddress"
-						value={clientAddress}
-						onChange={(e) => onChange(e)}
-						required
-					/>
-				</div>
-				<small>Note: This Page is not working right now.</small>
-				<h2 className="text-primary">AddPerson Component</h2>
-				<input type="submit" className="btn btn-primary my-1" />
-				<Link className="btn btn-light my-1" to="/dashboard">
-					Go Back
-				</Link>
-			</form>
-		</Fragment>
-	);
+const ClientForm = ({ createClient, history }) => {
+  return (
+    <div>
+      <h1 className="large text-primary">Create Client</h1>
+      <Formik
+        initialValues={{
+          clientName: "",
+          clientAddress: "",
+          personDetails: [
+            { id: Math.random(), name: "", email: "", phoneNumber: "" },
+          ],
+        }}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          createClient(data, history);
+          console.log("submit: ", data);
+          setSubmitting(false);
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <Form className="form">
+            <div className="form-group">
+              <Field type="text" name="clientName" placeholder="Client Name" />
+            </div>
+            <div className="form-group">
+              <Field
+                type="text"
+                name="clientAddress"
+                placeholder="Client Address"
+              />
+            </div>
+            <FieldArray name="personDetails">
+              {(arrayHelpers) => (
+                <div>
+                  <button
+                    className="btn btn-primary my-1"
+                    type="button"
+                    onClick={() =>
+                      arrayHelpers.push({
+                        name: "",
+                        email: "",
+                        phoneNumber: "",
+                        id: Math.random(),
+                      })
+                    }
+                  >
+                    Add Person
+                  </button>
+                  {values.personDetails.map((personDetail, index) => {
+                    return (
+                      <div key={personDetail.id}>
+                        <div className="form-group">
+                          <Field
+                            type="text"
+                            name={`personDetails.${index}.name`}
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <Field
+                            type="email"
+                            name={`personDetails.${index}.email`}
+                            placeholder="Email"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <Field
+                            type="number"
+                            name={`personDetails.${index}.phoneNumber`}
+                            placeholder="Phone Number"
+                          />
+                        </div>
+                        <button
+                          className="btn btn-primary my-1 "
+                          type="button"
+                          onClick={() => arrayHelpers.remove(index)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </FieldArray>
+            <input
+              type="submit"
+              className="btn btn-primary my-1"
+              disabled={isSubmitting}
+            />
+            <Link className="btn btn-light my-1" to="/dashboard">
+              Go Back
+            </Link>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
-export default connect(null)(withRouter(ClientForm));
+ClientForm.propTypes = {
+  createClient: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createClient })(withRouter(ClientForm));
