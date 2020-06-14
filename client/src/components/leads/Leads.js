0eac895860db440a0d1172f240671926806e2a51
Vikas_Kumar <svikas641@gmail.com>
2020-06-14 17:18:24 +0530

    added populate lead function

--- client/src/components/leads/Leads.js
+++ client/src/components/leads/Leads.js
@@ -1,41 +1,47 @@
-import React, {Fragment, useEffect} from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getLeads } from '../../actions/lead';
-import Spinner from '../layout/Spinner';
-import LeadItem from './LeadItem';
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getLeads } from "../../actions/lead";
+import { populateLeadData } from "../../actions/duty";
+import Spinner from "../layout/Spinner";
+import LeadItem from "./LeadItem";
 
-
-const Leads = ({ getLeads, lead: { leads, loading } }) => {
-	useEffect(() => {
+const Leads = ({
+  getLeads,
+  populateLeadData,
+  lead: { leads, lead, loading },
+}) => {
+  useEffect(() => {
     getLeads();
-  }, [getLeads]);
+    populateLeadData();
+  }, [getLeads, populateLeadData]);
 
-
-	return loading ? (<Spinner />) : (
-		<Fragment>
+  return loading || leads === [] ? (
+    <Spinner />
+  ) : (
+    <Fragment>
       <h1 className="large text-primary">All Leads</h1>
       <Link to="/dashboard" className="btn btn-dark my-1">
         Back To Dashboard
-      </Link>
-      {' '}
+      </Link>{" "}
       <div className="posts">
         {leads.map((lead) => (
           <LeadItem key={lead._id} lead={lead} />
-          ))}
+        ))}
       </div>
     </Fragment>
-	)
-}
+  );
+};
 
 Leads.propTypes = {
-	getLeads: PropTypes.func.isRequired,
-    lead: PropTypes.object.isRequired
-}
+  getLeads: PropTypes.func.isRequired,
+  populateLeadData: PropTypes.func.isRequired,
+  lead: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = (state) => ({
-  lead: state.lead
+  lead: state.lead,
 });
 
-export default connect(mapStateToProps, { getLeads })(Leads);
+export default connect(mapStateToProps, { getLeads, populateLeadData })(Leads);
