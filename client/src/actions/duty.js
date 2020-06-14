b10bb0978d36e4c4d4e922ca153c709dc27479cd
Vikas_Kumar <svikas641@gmail.com>
2020-06-14 17:20:22 +0530

    added populate lead fun

--- client/src/actions/duty.js
+++ client/src/actions/duty.js
@@ -2,70 +2,80 @@ import axios from "axios";
 import { setAlert } from "./alert";
 
 import {
-	ADD_ASSIGN_CLIENT,
-	ASSIGN_CLIENT_ERROR,
-	ADD_ASSIGN_PROSPECT,
-	ASSIGN_PROSPECT_ERROR,
+  ADD_ASSIGN_CLIENT,
+  ASSIGN_CLIENT_ERROR,
+  ADD_ASSIGN_PROSPECT,
+  ASSIGN_PROSPECT_ERROR,
+  GET_LEADS,
+  LEAD_ERROR,
 } from "./types";
 
 export const assignClient = (formData, history) => async (dispatch) => {
-	try {
-		const config = {
-			headers: {
-				"Content-Type": "application/json",
-			},
-		};
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-		const res = await axios.post(
-			"/api/duties/assignDuty",
-			formData,
-			config
-		);
-		dispatch({
-			type: ADD_ASSIGN_CLIENT,
-			payload: res.data,
-		});
+    const res = await axios.post("/api/duties/assignDuty", formData, config);
+    dispatch({
+      type: ADD_ASSIGN_CLIENT,
+      payload: res.data,
+    });
 
-		dispatch(setAlert("Client Duty Assigned", "success"));
-		history.push("/assignDuty");
-	} catch (err) {
-		dispatch({
-			type: ASSIGN_CLIENT_ERROR,
-			payload: {
-				msg: err.response,
-				status: err.response,
-			},
-		});
-	}
+    dispatch(setAlert("Client Duty Assigned", "success"));
+    history.push("/assignDuty");
+  } catch (err) {
+    dispatch({
+      type: ASSIGN_CLIENT_ERROR,
+      payload: {
+        msg: err.response,
+        status: err.response,
+      },
+    });
+  }
+};
+
+export const populateLeadData = () => async (dispatch) => {
+  try {
+    const res = await axios.get("api/lead/populateData");
+
+    dispatch({
+      type: GET_LEADS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LEAD_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
 };
 
 export const assignProspect = (formData, history) => async (dispatch) => {
-	try {
-		const config = {
-			headers: {
-				"Content-Type": "application/json",
-			},
-		};
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-		const res = await axios.post(
-			"/api/duties/assignDuty",
-			formData,
-			config
-		);
-		dispatch({
-			type: ADD_ASSIGN_PROSPECT,
-			payload: res.data,
-		});
+    const res = await axios.post("/api/duties/assignDuty", formData, config);
+    dispatch({
+      type: ADD_ASSIGN_PROSPECT,
+      payload: res.data,
+    });
 
-		dispatch(setAlert("Prospect Duty Assigned", "success"));
-		history.push("/assignDuty");
-	} catch (err) {
-		dispatch({
-			type: ASSIGN_PROSPECT_ERROR,
-			payload: {
-				msg: err.response,
-				status: err.response,
-			},
-		});
-	}
+    dispatch(setAlert("Prospect Duty Assigned", "success"));
+    history.push("/assignDuty");
+  } catch (err) {
+    dispatch({
+      type: ASSIGN_PROSPECT_ERROR,
+      payload: {
+        msg: err.response,
+        status: err.response,
+      },
+    });
+  }
 };
