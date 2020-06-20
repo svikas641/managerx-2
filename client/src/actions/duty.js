4f8ba6efe8488f62ed91f01c269932c31105b294
Vikas_Kumar <svikas641@gmail.com>
2020-06-20 21:00:38 +0530

    added assign duty and its error

--- client/src/actions/duty.js
+++ client/src/actions/duty.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import {
-  ADD_ASSIGN_CLIENT,
-  ASSIGN_CLIENT_ERROR,
-  ADD_ASSIGN_PROSPECT,
-  ASSIGN_PROSPECT_ERROR,
-  GET_LEADS,
-  LEAD_ERROR,
-} from "./types";
+import { ASSIGN_DUTY, ASSIGN_DUTY_ERROR, GET_LEADS, LEAD_ERROR } from "./types";
 
-export const assignClient = (formData, history) => async (dispatch) => {
+export const assignDuty = (formData, history) => async (dispatch) => {
   try {
     const config = {
       headers: {
@@ -20,15 +13,16 @@ export const assignClient = (formData, history) => async (dispatch) => {
 
     const res = await axios.post("/api/duties/assignDuty", formData, config);
     dispatch({
-      type: ADD_ASSIGN_CLIENT,
+      type: ASSIGN_DUTY,
       payload: res.data,
     });
 
-    dispatch(setAlert("Client Duty Assigned", "success"));
+    dispatch(setAlert("Checking Duty Assignment", "checking"));
+    dispatch(setAlert(`${res.data}`, "success"));
     history.push("/assignDuty");
   } catch (err) {
     dispatch({
-      type: ASSIGN_CLIENT_ERROR,
+      type: ASSIGN_DUTY_ERROR,
       payload: {
         msg: err.response,
         status: err.response,
@@ -49,33 +43,6 @@ export const populateLeadData = () => async (dispatch) => {
     dispatch({
       type: LEAD_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
-    });
-  }
-};
-
-export const assignProspect = (formData, history) => async (dispatch) => {
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    const res = await axios.post("/api/duties/assignDuty", formData, config);
-    dispatch({
-      type: ADD_ASSIGN_PROSPECT,
-      payload: res.data,
-    });
-
-    dispatch(setAlert("Prospect Duty Assigned", "success"));
-    history.push("/assignDuty");
-  } catch (err) {
-    dispatch({
-      type: ASSIGN_PROSPECT_ERROR,
-      payload: {
-        msg: err.response,
-        status: err.response,
-      },
     });
   }
 };
