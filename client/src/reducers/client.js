8341c23ccbd4b4bac47dbc5a006982e35a08d97c
Vikas_Kumar <svikas641@gmail.com>
2020-06-20 20:53:26 +0530

    removed prospect related reducers

--- client/src/reducers/client.js
+++ client/src/reducers/client.js
@@ -1,17 +1,12 @@
 import {
   GET_CLIENTS,
   CLIENTS_ERROR,
-  GET_PROSPECTS,
-  PROSPECTS_ERROR,
   ADD_CLIENT,
   ADD_CLIENT_ERROR,
-  ADD_PROSPECT,
-  ADD_PROSPECT_ERROR,
 } from "../actions/types";
 
 const initialState = {
   clients: [],
-  prospects: [],
   loading: true,
   error: {},
 };
@@ -26,14 +21,7 @@ export default function (state = initialState, action) {
         clients: payload,
         loading: false,
       };
-    case GET_PROSPECTS:
-      return {
-        ...state,
-        prospects: payload,
-        loading: false,
-      };
     case CLIENTS_ERROR:
-    case PROSPECTS_ERROR:
       return {
         ...state,
         error: payload,
@@ -45,13 +33,6 @@ export default function (state = initialState, action) {
         clients: [payload, ...state.clients],
         loading: false,
       };
-    case ADD_PROSPECT:
-      return {
-        ...state,
-        prospects: [payload, ...state.prospects],
-        loading: false,
-      };
-    case ADD_PROSPECT_ERROR:
     case ADD_CLIENT_ERROR:
       return {
         ...state,
