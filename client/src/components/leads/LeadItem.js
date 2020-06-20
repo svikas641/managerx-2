5dbcfc16aebaaf1d213500a5e24d6833f0ba1ab8
Vikas_Kumar <svikas641@gmail.com>
2020-06-20 20:59:28 +0530

    removed moment

--- client/src/components/leads/LeadItem.js
+++ client/src/components/leads/LeadItem.js
@@ -1,28 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import Moment from "react-moment";
 import { connect } from "react-redux";
 
-const LeadItem = ({ auth, lead: { _id, companyName, date } }) => (
-	<div>
-		<Link
-			to={`/leads-log/${_id}`}
-			className="list-group-item list-group-item-action"
-		>
-			<b>{companyName}</b> - Created on{" "}
-			<Moment format="DD/MM/YYYY">{date}</Moment>{" "}
-		</Link>
-	</div>
+const LeadItem = ({ auth, lead: { _id, clientName, date } }) => (
+  <div>
+    <Link
+      to={`/leads-log/${_id}`}
+      className="list-group-item list-group-item-action"
+    >
+      {clientName}
+    </Link>
+  </div>
 );
 
 LeadItem.propTypes = {
-	lead: PropTypes.object.isRequired,
-	auth: PropTypes.object.isRequired,
+  lead: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-	auth: state.auth,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, {})(LeadItem);
