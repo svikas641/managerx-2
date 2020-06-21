a5cb0445e57df7e3a0fb7430f1cf2d7d86ad9847
Vikas_Kumar <svikas641@gmail.com>
2020-06-21 13:49:38 +0530

    modified lists info

--- client/src/components/lead/VisitItem.js
+++ client/src/components/lead/VisitItem.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Moment from 'react-moment';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Moment from "react-moment";
 
 const VisitItem = ({
   leadId,
-  visit: { _id, status, commentBox, clientName, clientEmail, clientPhoneNumber, date },
-  auth
+  visit: { _id, status, commentBox, name, email, phoneNumber, date },
+  auth,
 }) => (
-    <div>
-      <div className='visit bg-white p-1 my-1'>
+  <div>
+    <div className="visit bg-white p-1 my-1">
       <div>
-      <ul className="list-group list-group-flush">
+        <ul className="list-group list-group-flush">
           <li> Status : {status}</li>
           <li> Comment : {commentBox}</li>
-          <li>Client Name : {clientName}</li>
-          <li>Client Email :  {clientEmail}</li>
-          <li>Client Phone Number : {clientPhoneNumber}</li>
+          <li>Client Name : {name}</li>
+          <li>Client Email : {email}</li>
+          <li>Client Phone Number : {phoneNumber}</li>
           <li>
-        Visited on <Moment format='DD/MM/YYYY'>{date}</Moment>
-      </li>
-    </ul>
+            Visited on <Moment format="DD/MM/YYYY">{date}</Moment>
+          </li>
+        </ul>
       </div>
-  </div>
+    </div>
   </div>
 );
 
@@ -32,10 +32,8 @@ VisitItem.propTypes = {
   auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
-export default connect(
-  mapStateToProps 
-)(VisitItem);
+export default connect(mapStateToProps)(VisitItem);
